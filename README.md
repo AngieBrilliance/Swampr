f2ffcbb901e3b6c531948bb7d97c27c62f4214f8
AngieBrilliance <angie@codeforprogress.org>
2014-06-04 09:49:50 -0400

    Adding Basic Blogging app in Flask

---
+++ README.md
@@ -0,0 +1,9 @@
+README.md
+======================================
+Swampr - A basic blogging app in Flask
+======================================
+This is an app we're using to learn Flask. It has some really basic components in it, and serves as a simple blog app:
+- Skeleton Flask files to launch an app (we began with "Hello, world!")
+- A models file that declares our classes (Post and User)
+- A forms file that defines forms for adding new users and adding new posts.
+- And Three views.
---
+++ app/views.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from app import app, db 	#Import our app components from other files
+from flask import Flask, render_template, redirect
+from models import Post, User
+from forms import NewUserForm
+
+@app.route('/')
+def index(): #This is the landing page
+all_users = User.query.all()
+posts = Post.query.all()
+return render_template("index.html", users = all_users, posts = posts)
+
+@app.route('/add_user', methods = ['GET', 'POST'])
+def add_user():
+	form = NewUserForm()
+	if form.validate_on_submit():
+		user = User()
+		form.populate_obj(user)
+		db.session.add(user)
+		db.session.commit()
+		return redirect('/')
+	return render_template("add_user.html", form = form)
